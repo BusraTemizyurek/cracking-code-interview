a6c8d7ab0bd997cb597c301614129337635738c6
Busra Temizyurek <busratemizyureek@gmail.com>
2024-12-03 11:55:39 -0800

    refactored

--- src/tree-and-graph/route-between-nodes-2.test.ts
+++ src/tree-and-graph/route-between-nodes-2.test.ts
@@ -1,6 +1,6 @@
 import { isRoute, TNode } from "./route-between-nodes-2";
 
-describe("isRoute returns true if a route exists between given nodes", () => {
+describe("isRoute", () => {
   const x = new TNode(1);
   const y = new TNode(2);
   const z = new TNode(3);
@@ -12,27 +12,27 @@ describe("isRoute returns true if a route exists between given nodes", () => {
   y.neighbors = [x, z, k];
   z.neighbors = [t];
 
-  it("isRoute returns true when the route exists between directly connected nodes", () => {
+  it("returns true when the route exists between directly connected nodes", () => {
     expect(isRoute(y, k)).toBe(true);
   });
 
-  it("isRoute returns false when the route doesn't exist between nodes, not connected in any way", () => {
+  it("returns false when the route doesn't exist between nodes, not connected in any way", () => {
     expect(isRoute(z, k)).toBe(false);
   });
 
-  it("isRoute returns true when source and destination are connected through an intermediate node.", () => {
+  it("returns true when source and destination are connected through an intermediate node.", () => {
     expect(isRoute(x, t)).toBe(true);
   });
 
-  it("isRoute returns true when graph contains a cycle, and a route exists to the destination.", () => {
+  it("returns true when graph contains a cycle, and a route exists to the destination.", () => {
     expect(isRoute(x, k)).toBe(true);
   });
 
-  it("isRoute returns true when route to self", () => {
+  it("returns true when route to self", () => {
     expect(isRoute(x, x)).toBe(true);
   });
 
-  it("isRoute returns false when the graph is disconnected", () => {
+  it("returns false when the graph is disconnected", () => {
     expect(isRoute(k, l)).toBe(false);
   });
 });
