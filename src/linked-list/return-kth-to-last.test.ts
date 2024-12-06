61c30d4bc7c5c7ee19b2601812924bfdf9672377
Busra Temizyurek <busratemizyureek@gmail.com>
2024-12-06 12:20:28 -0800

    refactored

--- src/linked-list/return-kth-to-last.test.ts
+++ src/linked-list/return-kth-to-last.test.ts
@@ -19,45 +19,45 @@ const list: ListNode<number> = {
 };
 
 describe("findKthToLast1", () => {
-  test("kth = 1 (last element)", () => {
+  it("kth = 1 (last element)", () => {
     expect(findKthToLast1(list, 1)?.val).toBe(5);
   });
 
-  test("kth = 3 (third to last element)", () => {
+  it("kth = 3 (third to last element)", () => {
     expect(findKthToLast1(list, 3)?.val).toBe(3);
   });
 
-  test("kth = 5 (first element)", () => {
+  it("kth = 5 (first element)", () => {
     expect(findKthToLast1(list, 5)?.val).toBe(1);
   });
 
-  test("kth = 6 (out of bounds)", () => {
+  it("kth = 6 (out of bounds)", () => {
     expect(findKthToLast1(list, 6)).toBeUndefined();
   });
 
-  test("kth = 0 (invalid kth)", () => {
+  it("kth = 0 (invalid kth)", () => {
     expect(findKthToLast1(list, 0)).toBeUndefined();
   });
 });
 
 describe("findKthToLast2", () => {
-  test("kth = 1 (last element)", () => {
+  it("kth = 1 (last element)", () => {
     expect(findKthToLast2(list, 1)?.val).toBe(5);
   });
 
-  test("kth = 3 (third to last element)", () => {
+  it("kth = 3 (third to last element)", () => {
     expect(findKthToLast2(list, 3)?.val).toBe(3);
   });
 
-  test("kth = 5 (first element)", () => {
+  it("kth = 5 (first element)", () => {
     expect(findKthToLast2(list, 5)?.val).toBe(1);
   });
 
-  test("kth = 6 (out of bounds)", () => {
+  it("kth = 6 (out of bounds)", () => {
     expect(findKthToLast2(list, 6)).toBeUndefined();
   });
 
-  test("kth = 0 (invalid kth)", () => {
+  it("kth = 0 (invalid kth)", () => {
     expect(findKthToLast2(list, 0)).toBeUndefined();
   });
 });
