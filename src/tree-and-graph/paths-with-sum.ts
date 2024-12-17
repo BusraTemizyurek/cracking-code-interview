5a28bd59182d9cbc9e905239c892a74bd0541489
Busra Temizyurek <busratemizyureek@gmail.com>
2024-12-17 10:04:05 -0800

    nq: tree&graph - paths-with-sum (#11)

---
+++ src/tree-and-graph/paths-with-sum.ts
@@ -0,0 +1,48 @@
+// page 123(111) Q-4.12
+
+import { BTNode } from "../types";
+
+function getPathsWithSum(
+  root: BTNode<number> | undefined,
+  sum: number,
+  path: BTNode<number>[],
+  pathsWithSum: BTNode<number>[][]
+) {
+  if (!root) {
+    return;
+  }
+  path.push(root);
+  const remain = sum - root.val;
+  if (remain === 0) {
+    pathsWithSum.push([...path]);
+  }
+  getPathsWithSum(root.left, remain, path, pathsWithSum);
+  getPathsWithSum(root.right, remain, path, pathsWithSum);
+  path.pop();
+
+  return;
+}
+
+export function findPathsWithsum(root: BTNode<number>, sum: number): number {
+  const pathsWithSum: BTNode<number>[][] = [];
+  inorder(root, sum, pathsWithSum);
+
+  return pathsWithSum.length;
+}
+
+function inorder(
+  root: BTNode<number> | undefined,
+  sum: number,
+  pathsWithSum: BTNode<number>[][]
+) {
+  if (!root) {
+    return;
+  }
+
+  inorder(root.left, sum, pathsWithSum);
+
+  const path: BTNode<number>[] = [];
+  getPathsWithSum(root, sum, path, pathsWithSum);
+
+  inorder(root.right, sum, pathsWithSum);
+}
