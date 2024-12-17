28650014e77a545240c3ec06270677321ab01329
Busra Temizyurek <busratemizyureek@gmail.com>
2024-12-17 09:00:14 -0800

    new question added to tree and graph category - paths-with-sum

---
+++ src/tree-and-graph/paths-with-sum.test.ts
@@ -0,0 +1,59 @@
+import { main } from "./paths-with-sum";
+import { BTNode } from "../types";
+
+describe("main function", () => {
+  it("works for simple tree", () => {
+    const root1: BTNode<number> = {
+      val: 5,
+      left: { val: 3, left: { val: 2 }, right: { val: 1 } },
+      right: { val: -2, left: { val: 1 }, right: { val: 6 } },
+    };
+    const sum1 = 8;
+
+    expect(main(root1, sum1)).toBe(1);
+  });
+
+  it("works for tree with multiple paths", () => {
+    const root2: BTNode<number> = {
+      val: 10,
+      left: {
+        val: 5,
+        left: { val: 3, left: { val: 3 }, right: { val: -2 } },
+        right: { val: 2, right: { val: 1 } },
+      },
+      right: {
+        val: -3,
+        right: { val: 11 },
+      },
+    };
+    const sum2 = 8;
+
+    expect(main(root2, sum2)).toBe(3);
+  });
+
+  it("works for tree with no path", () => {
+    const root3: BTNode<number> = {
+      val: 1,
+      left: { val: 2 },
+      right: { val: 3 },
+    };
+    const sum3 = 10;
+
+    expect(main(root3, sum3)).toBe(0);
+  });
+
+  it("works for tree with just 1 node in path", () => {
+    const root4: BTNode<number> = {
+      val: -10,
+      left: { val: 9 },
+      right: {
+        val: 20,
+        left: { val: 15 },
+        right: { val: 7 },
+      },
+    };
+    const sum4 = 7;
+
+    expect(main(root4, sum4)).toBe(1);
+  });
+});
