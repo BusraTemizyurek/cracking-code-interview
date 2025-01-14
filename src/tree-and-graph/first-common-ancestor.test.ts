c3466f908fbce9a72f19d49f7d2c3ec13c744ba8
Busra Temizyurek <busratemizyureek@gmail.com>
2025-01-14 11:01:50 -0800

    refactored

--- src/tree-and-graph/first-common-ancestor.test.ts
+++ src/tree-and-graph/first-common-ancestor.test.ts
@@ -31,7 +31,6 @@ describe("findFirstCommonAncestor", () => {
     n3.right = n6;
     n6.right = n7;
 
-    //expect(findFirstCommonAncestor(n1, n7, n5)).toBe(n1);
     expect(findFirstCommonAncestor(n1, n4, n5)).toBe(n2);
   });
 
